1fe2ae3f3ac6172c3e57d107440de5a6c685543c
Vin <130062494+AlvinOngg@users.noreply.github.com>
2025-01-09 13:19:14 +0700

    TC026 & TC027

--- demoapp/src/test/java/com/example/selenium/App6CarRentalTest.java
+++ demoapp/src/test/java/com/example/selenium/App6CarRentalTest.java
@@ -46,7 +46,7 @@ public class App6CarRentalTest {
 
     @Test
     @Feature("TC025 Success Search Car Rental")
-    public void TC025_testSuccessSearchCarRental(){
+    public void TC025_testSuccessSearchCarRental() {
         String startTime = getCurrentTimestamp();
         attachTimestamp("Test Start Time", startTime);
 
@@ -69,7 +69,53 @@ public class App6CarRentalTest {
         searchCarRentalButton();
         verifySuccessSearchCarRental();
         takeScreenshot("TC025_testSuccessSearchCarRental");
+        resetCarRentalPage();
 
+        String endTime = getCurrentTimestamp();
+        attachTimestamp("Test End Time", endTime);
+    }
+    
+    @Test
+    @Feature("TC026 Success Search Car Rental( Drop Off Different Location )")
+    public void TC026_testSuccessSearchCarRentallDropOffDiffLocation() {
+        String startTime = getCurrentTimestamp();
+        attachTimestamp("Test Start Time", startTime);
+
+        clickDropOffAtDiffLocation();
+        clickPickUpLocation();
+        selectPickUpLocation();
+        clickDropOffLocation();
+        selectDropOffLocation();
+        clickPickUpDate();
+        selectPickUpDate();
+        clickPickUpHour();
+        selectPickUpHour();
+        clickDropOffDate();
+        selectDropOffDate();
+        clickDropOffHour();
+        selectDropOffHour();
+        clickDriverLicenceCountry();
+        selectDriverLicenceCountry();
+        clickAge();
+        selectAge();
+        searchCarRentalButton();
+        verifySuccessSearchCarRentalDiffLocation();
+        takeScreenshot("TC026_testSuccessSearchCarRentallDropOffDiffLocation");
+        resetCarRentalPage();
+
+        String endTime = getCurrentTimestamp();
+        attachTimestamp("Test End Time", endTime);
+    }
+
+    @Test
+    @Feature("TC027 Failed Search Cars Rentals (dont do anything with the field)")
+    public void TC027_testFailedSearchCarsRentals() {
+        String startTime = getCurrentTimestamp();
+        attachTimestamp("Test Start Time", startTime);
+        searchCarRentalButton();
+        verifyFailSearchCarRental();
+        takeScreenshot("TC027_testFailedSearchCarsRentals");
+        resetCarRentalPage();
         String endTime = getCurrentTimestamp();
         attachTimestamp("Test End Time", endTime);
     }
@@ -107,6 +153,27 @@ public class App6CarRentalTest {
                         " #__next > div.car-search-form-wrapper > div > div.title-wrapper > div.car-card.large.gray.car-search-form > div.car-search-content > div.car-location-form-wrapper > div > div > div > div.adm-popup.car-dropdown-popup.location-select-item-wrapper > div > div > div > div > div.select-item-wrapper.margin-top-196 > div:nth-child(1) > div.locations-content > div:nth-child(4) > div.car-select-item.select-item-suguest")));
         pickUpLocation.click();
     }
+
+
+    @Step("Click Drop Off Location")
+    private void clickDropOffLocation() {
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
+        WebElement pickUpLocation = wait
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                        "#__next > div.car-search-form-wrapper > div > div.title-wrapper > div.car-card.large.gray.car-search-form > div.car-search-content > div.car-location-form-wrapper > div > div:nth-child(3) > div > div > div > div")));
+        pickUpLocation.click();
+    }
+      
+    @Step("Select Drop Off Location")
+    private void selectDropOffLocation() {
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
+        WebElement pickUpLocation = wait
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                        " #__next > div.car-search-form-wrapper > div > div.title-wrapper > div.car-card.large.gray.car-search-form > div.car-search-content > div.car-location-form-wrapper > div > div:nth-child(3) > div > div.adm-popup.car-dropdown-popup.location-select-item-wrapper > div > div > div > div > div.select-item-wrapper.margin-top-196 > div:nth-child(3) > div.locations-content > div:nth-child(3) > div.car-select-item.select-item-suguest")));
+        pickUpLocation.click();
+    }
+
+    
 
     @Step("Click Pick Up Date")
     private void clickPickUpDate() {
@@ -227,23 +294,76 @@ public class App6CarRentalTest {
     @Step("Verify Success Search Car Rental")
     private void verifySuccessSearchCarRental() {
         driver = app.getDriver();
-        
+
         // Wait for the new tab or page to load
         WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
-        
+
         // Switch to the newly opened tab if applicable
         for (String handle : driver.getWindowHandles()) {
             driver.switchTo().window(handle);
         }
-    
+
         By successElementSelector = By.cssSelector(
                 "#sticky-container > div > div.car-page-inner > div > div.car-page-main > div.car-progress > div");
-        
+
         // Wait for the element to be visible
         WebElement successElement = wait.until(ExpectedConditions.visibilityOfElementLocated(successElementSelector));
-        
+
         // Assert the element is displayed
         Assert.assertTrue(successElement.isDisplayed(), "Success element not displayed!");
+    }
+
+    @Step("Verify Failed Search Car Rental")
+    private void verifyFailSearchCarRental() {
+        driver = app.getDriver();
+
+        // Wait for the new tab or page to load
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+
+        // Switch to the newly opened tab if applicable
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+
+        By successElementSelector = By.cssSelector(
+                "#sticky-container > div > div.car-page-inner > div > div.car-page-main > div.car-list-bar-wrapper > div.car-filter-bar > div.car-filter-bar-content > div > div > div > button");
+
+        // Wait for the element to be visible
+        WebElement successElement = wait.until(ExpectedConditions.visibilityOfElementLocated(successElementSelector));
+
+        // Assert the element is displayed
+        Assert.assertTrue(successElement.isDisplayed(), "Success element not displayed!");
+    }
+
+    @Step("Verify Success Search Car Rental Drop Off Different Location")
+    private void verifySuccessSearchCarRentalDiffLocation() {
+        driver = app.getDriver();
+
+        // Wait for the new tab or page to load
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+
+        // Switch to the newly opened tab if applicable
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+
+        By successElementSelector = By.cssSelector(
+                "#sticky-container > div > div.car-page-inner > div > div.car-page-main > div.car-vehicle-list > div:nth-child(1) > div > div > div.car-vendor-price > div:nth-child(1) > div.center-vendor > div > div.car-term-item > div.text.sub-text");
+
+        // Wait for the element to be visible
+        WebElement successElement = wait.until(ExpectedConditions.visibilityOfElementLocated(successElementSelector));
+
+        // Assert the element is displayed
+        Assert.assertTrue(successElement.isDisplayed(), "Success element not displayed!");
+    }
+    
+    @Step("Click Drop Off at Different Location")
+    private void clickDropOffAtDiffLocation() {
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
+        WebElement diffLocation = wait
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                        "#__next > div.car-search-form-wrapper > div > div.title-wrapper > div.car-card.large.gray.car-search-form > div:nth-child(2) > div > div.car-checkbox.different-location-check-box > div")));
+        diffLocation.click();
     }
     
     @Step("Take Screenshot")
