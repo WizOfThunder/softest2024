564e961e5774fd203ef31030f2036488c4e72b0d
Vin <130062494+AlvinOngg@users.noreply.github.com>
2025-01-21 11:33:48 +0700

    add TC049 - TC051

--- demoapp/src/test/java/com/example/selenium/App09CruiseTest.java
+++ demoapp/src/test/java/com/example/selenium/App09CruiseTest.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.testng.annotations.AfterClass;
@@ -21,6 +23,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -79,12 +82,80 @@ public class App09CruiseTest {
         attachTimestamp("Test End Time", endTime);
     }
 
+    @Test
+    @Feature("TC049 Failed Book Cruise No Input")
+    public void TC049_testFailBookCruiseNoInput() {
+        String startTime = getCurrentTimestamp();
+        attachTimestamp("Test Start Time", startTime);
+
+        setUpCruiseBook();
+        selectCruise(3);
+        clickSelectRoom(4);
+        selectCruiseRoom();
+        clickPayNow();
+        verifyFailedBookCruiseNoInput();
+        takeScreenshot("TC049_testFailBookCruiseNoInput");
+        resetCruisePage();
+
+        String endTime = getCurrentTimestamp();
+        attachTimestamp("Test End Time", endTime);
+    }
+    
+    @Test
+    @Feature("TC050 Failed Book Cruise Invalid Email")
+    public void TC050_testFailBookCruiseInvalidEmail() {
+        String startTime = getCurrentTimestamp();
+        attachTimestamp("Test Start Time", startTime);
+
+        setUpCruiseBook();
+        selectCruise(5);
+        clickSelectRoom(6);
+        selectCruiseRoom();
+        inputBookingField("akun", "akun", "85246888888", 0);
+        clickPayNow();
+        verifyFailedBookCruiseInvalidEmail();
+        takeScreenshot("TC050_testFailBookCruiseInvalidEmail");
+        resetCruisePage();
+
+        String endTime = getCurrentTimestamp();
+        attachTimestamp("Test End Time", endTime);
+    }
+
+    @Test
+    @Feature("TC051 Failed Book Cruise Invalid Phone")
+    public void TC051_testFailBookCruiseInvalidPhone() {
+        String startTime = getCurrentTimestamp();
+        attachTimestamp("Test Start Time", startTime);
+
+        setUpCruiseBook();
+        selectCruise(7);
+        clickSelectRoom(8);
+        selectCruiseRoom();
+        inputBookingField("akun", "akunistts@gmail.com", "", 0);
+        clickPayNow();
+        verifyFailedBookCruiseInvalidPhone();
+        takeScreenshot("TC051_testFailBookCruiseInvalidPhone");
+
+        String endTime = getCurrentTimestamp();
+        attachTimestamp("Test End Time", endTime);
+    }
+
     @Step("Navigate to cruise page")
     private void navigateToCruisePage() {
         WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
         WebElement cruisePage = wait
                 .until(ExpectedConditions.elementToBeClickable(By.cssSelector("#header_action_nav_cruises")));
         cruisePage.click();
+    }
+
+    @Step("SetUp Cruise Book")
+    private void setUpCruiseBook() {
+        clickDepartFrom();
+        selectDepartFrom();
+        clickMonthOfDeparture();
+        selectMonthOfDeparture();
+        searchCruiseButton();
+        
     }
 
     @Step("Click Depart From")
@@ -148,6 +219,87 @@ public class App09CruiseTest {
         WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
                 "#__next > div.cruise-container > div.home-head > div.cruise-content.top-banner > div > div > ul > li.search-form-btn > button")));
         searchButton.click();
+    }
+
+    @Step("Select Cruise")
+    private void selectCruise(int index) {
+
+        List<String> windowHandles = new ArrayList<>(app.getDriver().getWindowHandles());
+        driver.switchTo().window(windowHandles.get(index));
+
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
+        WebElement cruise = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                "#__next > div.cruise-container > div.cruise-list-page > div.cruise-content > div > div.list-main > div.product-list > div:nth-child(1) > div")));
+        cruise.click();
+    }
+
+    @Step("Click Select Cruise")
+    private void clickSelectRoom(int index) {
+        List<String> windowHandles = new ArrayList<>(app.getDriver().getWindowHandles());
+        driver.switchTo().window(windowHandles.get(index));
+
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                "#ibucru-10650039188-info-select-367340")));
+        button.click();
+    }
+
+    @Step("Select Cruise Room")
+    private void selectCruiseRoom() {
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
+        Actions actions = new Actions(app.getDriver());
+
+        WebElement showMore = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                "#ibucru-10650039190-room-spread-367340-21687-1-s")));
+        showMore.click();
+
+        WebElement plusButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                "#ibucru-10650039190-room-num-367340-76026455-room-y > i")));
+        plusButton.click();
+
+        WebElement nextButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                "#ibucru-10650039190-room-next-367340 > button")));
+        actions.moveToElement(nextButton).perform();
+        ;
+        nextButton.click();
+    }
+
+    @Step("Click Book")
+    private void clickPayNow() {
+        delay(3000);
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(30));
+        WebElement pay = wait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector(
+                                "#ibucru-10650039186-trackorder-payway > button")));
+        Actions actions = new Actions(app.getDriver());
+        actions.moveToElement(pay).perform();
+        pay.click();
+    }
+
+    @Step("input Booking Field")
+    private void inputBookingField(String Name, String email, String phone,int index) {
+        WebDriverWait wait = new WebDriverWait(app.getDriver(), java.time.Duration.ofSeconds(10));
+        WebElement nameField = wait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector("#ibucru-10650039186-passager-box-name > div.input-box-component_base_input_container.blur > div > input[type=text]")));
+        Actions actions = new Actions(app.getDriver());
+        actions.moveToElement(nameField);
+        nameField.clear();
+        nameField.sendKeys(Name);
+
+
+        WebElement emailField = wait
+            .until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector("#ibucru-10650039186-passager-box-email > div.input-box-component_base_input_container.with-prefix-icon.blur > div > input[type=text]")));
+        emailField.clear();
+        emailField.sendKeys(email);
+                        
+        WebElement phoneField = wait
+            .until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector("#J-contactInfo > div > div:nth-child(2) > div > div > div > div:nth-child(1) > div > div.input-box-component_base_input_container.blur > div > input[type=text]")));
+        phoneField.clear();
+        phoneField.sendKeys(phone);
     }
 
     @Step("Take Screenshot")
@@ -245,6 +397,72 @@ public class App09CruiseTest {
 
         By successElementSelector = By.cssSelector(
                 "#__next > div.cruise-container > div.cruise-list-page > div.cruise-content > div > div.list-main > div.filter-result-box > div.filter-result > div > div > span");
+
+        // Wait for the element to be visible
+        WebElement successElement = wait.until(ExpectedConditions.visibilityOfElementLocated(successElementSelector));
+
+        // Assert the element is displayed
+        Assert.assertTrue(successElement.isDisplayed(), "Success element not displayed!");
+    }
+
+    @Step("Verify Fail Book Cruise No Input")
+    private void verifyFailedBookCruiseNoInput() {
+        driver = app.getDriver();
+
+        // Wait for the new tab or page to load
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+
+        // Switch to the newly opened tab if applicable
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+
+        By successElementSelector = By.cssSelector(
+                "#ibucru-10650039186-passager-box-name > div.input-box-component_base_bottom_container > div");
+
+        // Wait for the element to be visible
+        WebElement successElement = wait.until(ExpectedConditions.visibilityOfElementLocated(successElementSelector));
+
+        // Assert the element is displayed
+        Assert.assertTrue(successElement.isDisplayed(), "Success element not displayed!");
+    }
+
+    @Step("Verify Fail Book Cruise Invalid Email")
+    private void verifyFailedBookCruiseInvalidEmail() {
+        driver = app.getDriver();
+
+        // Wait for the new tab or page to load
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+
+        // Switch to the newly opened tab if applicable
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+
+        By successElementSelector = By.cssSelector(
+                "#ibucru-10650039186-passager-box-email > div.input-box-component_base_bottom_container > div");
+
+        // Wait for the element to be visible
+        WebElement successElement = wait.until(ExpectedConditions.visibilityOfElementLocated(successElementSelector));
+
+        // Assert the element is displayed
+        Assert.assertTrue(successElement.isDisplayed(), "Success element not displayed!");
+    }
+
+    @Step("Verify Fail Book Cruise Invalid Phone")
+    private void verifyFailedBookCruiseInvalidPhone() {
+        driver = app.getDriver();
+
+        // Wait for the new tab or page to load
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+
+        // Switch to the newly opened tab if applicable
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+
+        By successElementSelector = By.cssSelector(
+                "#J-contactInfo > div > div:nth-child(2) > div > div > div > div:nth-child(1) > div > div.input-box-component_base_bottom_container > div");
 
         // Wait for the element to be visible
         WebElement successElement = wait.until(ExpectedConditions.visibilityOfElementLocated(successElementSelector));
